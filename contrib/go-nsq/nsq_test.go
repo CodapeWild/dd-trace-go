e5126d6283712332501be9e6faaa074572e859e2
CodapeWild <codape.wild@gmail.com>
2022-12-19 13:59:17 +0800

    fix

--- contrib/go-nsq/nsq_test.go
+++ contrib/go-nsq/nsq_test.go
@@ -6,8 +6,59 @@
 
 package nsq
 
-import "testing"
+import (
+	"fmt"
+	"io"
+	"net"
+	"testing"
+	"time"
+)
 
-func TestNsq(t *testing.T) {
+type instruction struct {
+	delay time.Duration
+}
 
+type mockNSQD struct {
+	t *testing.T
+}
+
+func TestTCPComm(t *testing.T) {
+	listner, err := net.Listen("tcp", "localhost:8080")
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	go func() {
+		for {
+			conn, err := listner.Accept()
+			if err != nil {
+				t.Error(err.Error())
+				time.Sleep(time.Second)
+				continue
+			}
+			fmt.Printf("connection: %s", conn.RemoteAddr().String())
+
+			go func() {
+				for {
+					buf := make([]byte, 1024)
+					if _, err := conn.Read(buf); err != nil {
+						fmt.Println(err.Error())
+					} else {
+						fmt.Println(string(buf))
+					}
+				}
+			}()
+		}
+	}()
+
+	conn, err := net.Dial("tcp", "localhost:8080")
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	for i := 0; i < 10; i++ {
+		if _, err := io.WriteString(conn, "hello, world "); err != nil {
+			fmt.Printf("write stirng through conn failed: %s\n", err.Error())
+		}
+	}
+
+	select {}
 }
